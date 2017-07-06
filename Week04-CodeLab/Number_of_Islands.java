81456b1884006f46826546d1462f2a75db0b2d3f
TravisCai <linsencai@hotmail.com>
2017-07-06 11:17:09 +0800

    Create  Number_of_Islands.java

---
+++ Week04-CodeLab/Number_of_Islands.java
@@ -0,0 +1,54 @@
+/*
+  Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+
+11000
+11000
+00100
+00011
+Answer: 3
+*／
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0){
+            return 0;
+        }
+        
+        int num = 0;
+        
+        for(int i = 0;i <  grid.length;i++)
+            for(int j = 0; j < grid[0].length;j++){
+                if(grid[i][j] == '0'){
+                    continue;
+                }
+                helper(grid,i,j);
+                num++;
+            }
+        
+        return num;
+        
+    }
+    private void helper(char[][] grid,int x,int y){
+        if(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] == '0'){
+            return;
+        }
+        grid[x][y] = '0';
+        
+        helper(grid,x-1,y);
+        helper(grid,x+1,y);
+        helper(grid,x,y+1);
+        helper(grid,x,y-1);
+        
+        return;
+    }
+}
