7fea4d790b274482c56349d614a9037328873d03
TravisCai <linsencai@hotmail.com>
2017-07-24 20:11:25 +0800

    Create 259_3_Sum_Smaller.java

---
+++ Week8/Tuesday/259_3_Sum_Smaller.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        int count = 0;
+        Arrays.sort(nums);
+        
+        for(int i = 0;i < nums.length - 2;i++){
+            int start = i + 1;
+            int end = nums.length - 1;
+            while(start < end){
+                if(nums[i] + nums[start] + nums[end] < target){
+                    count += end - start;
+                    start++;
+              }else{
+                    end --;
+                }
+            }
+        }
+ 
+    return count;
+}
+}
